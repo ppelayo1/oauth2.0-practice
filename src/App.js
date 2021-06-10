d57761143fb413235752ae32d4ec2b7ffcfbd20d
Patrick Pelayo <PatrickP@PatrickP.Tech>
2021-06-09 17:40:12 -0700

    got front-end login working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+/* global gapi */
 import logo from './logo.svg';
 import './App.css';
+import {GoogleLogin, GoogleLogout  }from 'react-google-login';
 
 function App() {
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  const logout = (response) => {
+    console.log(response, 'success');
+  }
+
+  const logoutFailure = (response) => {
+    console.log(response, 'failure');
+  }
+
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      alkshjaohotiwerhto
+      <GoogleLogin
+        clientId="794344810158-e00o3v3254fdo1aiujnpt2675rjc54r8.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        isSignedIn={false}
+        cookiePolicy={'single_host_origin'}
+        jsSrc={"  https://apis.google.com/js/api.js"}
+      />,
+      document.getElementById('googleButton')
+    );
+
+    <GoogleLogout
+      clientId="794344810158-e00o3v3254fdo1aiujnpt2675rjc54r8.apps.googleusercontent.com"
+      buttonText="Logout"
+      onLogoutSuccess={logout}
+      onFailure={logoutFailure}
+      jsSrc={"https://apis.google.com/js/api.js"}
+    >
+    </GoogleLogout>
     </div>
   );
 }
